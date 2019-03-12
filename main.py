967c837c315f8ddb6201ab31f7eb34aed7a01652
mobiusKey <wallacemark77@gmail.com>
2019-03-12 18:49:40 -0400

    drawing map and plotting locations

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from mpl_toolkits.basemap import Basemap
 import socket
 from urllib.request import urlopen
 import json
-
+import matplotlib.pyplot as plt
 
 
 file = open('botnet-capture-20110815-fast-flux.pcap', 'rb')
@@ -22,6 +22,10 @@ packet = dpkt.pcap.Reader(file)
 addresses = {}
 locations = {}
 
+all_addresses = []
+all_sources = []
+all_destinations = []
+
 # ts is the timestamp and buf contains everything else
 for ts, buf in packet:
 	eth = dpkt.ethernet.Ethernet(buf)
@@ -29,29 +33,38 @@ for ts, buf in packet:
 		ip = eth.data
 		addresses[socket.inet_ntoa(ip.src)] = socket.inet_ntoa(ip.dst)
 		
+for src, dst in addresses.items():
+	if src not in all_addresses:
+		all_addresses.append(src)
+	if dst not in all_addresses:
+		all_addresses.append(dst)
 
 def getInput():
 	while(True):
 		answer = input("|>")
 		answer = answer.lower()
-		if answer is None or "help in answer":
+		if answer is None or "help" in answer:
 			help()
 		if "print" in answer:
 			if "addresses" in answer:
 				printAddresses()
 			if "location" in answer:
 				printLocations()
-				
+		if "draw" in answer:
+			drawMap()
+		if "exit" in answer:
+			exit()
 def help():
 	print("TODO: add help")
 	
 def printAddresses():
 	for src, dst in addresses.items():
 		print(str(src) + " -> " + str(dst))
+
 		
 def getLocations():
 	print("getting locations...")
-	for x in addresses:
+	for x in all_addresses:
 		url = 'http://www.geoip-db.com/json/{0}'.format(x)
 		response = urlopen(url)
 		data = json.load(response)
@@ -61,5 +74,30 @@ def printLocations():
 	getLocations()
 	for x, y in locations.items():
 		print(str(x) + ": " + str(y["country_name"]))
+
+# should eventually make this a class and handle all the graphics with that
+def drawMap():
+	if locations:
+		print("drawing map...")
+		coordinates = []
+		
+		for ip, loc_data in locations.items():
+			if loc_data["latitude"] != "Not found":
+				coordinates.append([ip, loc_data["latitude"], loc_data["longitude"]])	
+				
+		fig = plt.figure(figsize=(10, 8), edgecolor='w')		
+		map = Basemap(projection='mill', llcrnrlat = -90, llcrnrlon = -180, urcrnrlat = 90, urcrnrlon = 180, resolution='l')
+		map.drawcoastlines()
+		map.drawcountries(linewidth=2)
+		map.bluemarble()
+		for loc in coordinates:	
+			ip, lat, lon = loc
+			xpt, ypt = map(lon, lat)
+			map.plot(xpt, ypt, 'co', markersize=8)
+
+		plt.show()
+	else:
+		getLocations()
+		drawMap()
 
 getInput()
